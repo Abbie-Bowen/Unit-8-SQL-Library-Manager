dd3cbe4cbc8bd98906a1f71deb3bf1d95aa573e3
Abbie <77949813+Abbie-Bowen@users.noreply.github.com>
2021-09-21 14:48:42 -0700

    book model created, database linked, started creating views

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ var router = express.Router();
 const Book = require('../models').Book;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  (async () => {
-    try {
+router.get('/', async(req, res, next)  => {
+  try {
+      const book = await Book.create({
+        title: 'Where to Begin',
+        author: 'Cleo Wade',
+        genre: 'poetry',
+        year: '2019',
+      });
       const books = await Book.findAll();
-      res.render('index', { title: 'Express', books: books.toJSON()});
+      res.json(books);
     } catch (error) {
       console.error(error);
     }
-  })
   // res.render('index', { title: 'Express' });
 });
 
