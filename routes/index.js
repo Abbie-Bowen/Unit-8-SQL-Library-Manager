36e0ec53149d73c72884aec2c54180c45bf656cc
Abbie <77949813+Abbie-Bowen@users.noreply.github.com>
2021-09-23 19:58:56 -0700

    all routes displaying, search and pagination working

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,88 @@
 var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
+const {Op} = require('../models').Sequelize;
 
-/* GET home page. */
+/* GET home page, forward to /books. */
 router.get('/', function(req, res){
-  res.redirect('/books');
+  res.redirect('/books/page-1');
 });
 
 /* GET all books */
-router.get('/books', async(req, res, next)  => {
+router.get('/books/view-all', async(req, res, next)  => {
   try {
-      const books = await Book.findAll({
-        attributes: ['id', 'title', 'author', 'genre', 'year']
-      });
-      res.render('index', {books: books});
+      const books = await Book.findAll();
+      let pages = 0;
+      res.render('books', {books: books, pages: pages});
     } catch (error) {
       next(error);
     }
 });
+
+/* GET books by page number */
+router.get('/books/page-:number', async(req, res, next)  => {
+  let page = req.params.number; //current page number
+  let limit = 10; //number of records per page
+  let offset = limit * (page - 1);
+  try {
+      let data = await Book.findAndCountAll();
+      let pages = Math.ceil(data.count / limit);
+
+      let books = await Book.findAll({
+        limit: limit,
+        offset: offset,
+      });
+      res.render('books', {books: books, pages: pages});
+    } catch (error) {
+      next(error);
+    }
+});
+
+/* GET search results */
+router.get('/search?:search', async(req, res, next) => {
+  try {
+    const searchText = req.query.search;
+    const results = await Book.findAll({
+      where: {
+        [Op.or]: [
+          {
+            title: {
+              [Op.like]: `%${searchText}%`
+            }
+          },
+          {
+            author: {
+              [Op.like]: `%${searchText}%`
+            }
+          },
+          {
+            genre: {
+              [Op.like]: `%${searchText}%`
+            }
+          },
+            {
+            year: {
+              [Op.like]: `%${searchText}%`
+            }
+          }
+        ]
+      }
+    });
+    if (results.length>0) {
+      let endSearch = 'End Search';
+      res.render('books', {books: results, endSearch: endSearch});
+    } else {
+      res.redirect('/search/no-results')
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* GET create a route for no search results */
+router.get('/search/no-results', (req, res) =>{
+  res.render('search-error');
+})
 
 /* GET create a new book form */
 router.get('/books/new', (req, res) => {
@@ -25,7 +90,7 @@ router.get('/books/new', (req, res) => {
 });
 
 /* GET individual book details */
-router.get('/books/:id', async(req, res, next) => {
+router.get('/books/view/:id', async(req, res, next) => {
   try {
     const book = await Book.findByPk(req.params.id);
     if (book) {
@@ -42,17 +107,17 @@ router.get('/books/:id', async(req, res, next) => {
 });
 
 /* POST update a book in database */
-router.post('/books/:id/edit', async(req, res) => {
+router.post('/books/edit/:id', async(req, res) => {
   let book;
   try {
     book = await Book.findByPk(req.params.id);
     if (book) {
       await book.update(req.body);
-      res.redirect('/books');
+      res.redirect('/books/page-1');
     } else {
       const err = new Error;
       err.status = 404;
-      err.message = "Bummer, it looks like the book you are trying to update doesn't exist."
+      err.message = "It looks like the book you are trying to update doesn't exist."
       next(err);
     }
   } catch (error) {
@@ -67,11 +132,11 @@ router.post('/books/:id/edit', async(req, res) => {
 })
 
 /* POST new book to database */
-router.post('/books', async(req, res) => {
+router.post('/books/new', async(req, res) => {
   let book;
   try{
     book = await Book.create(req.body);
-    res.redirect('/books');
+    res.redirect('/books/page-1');
   } catch (error) {
     if (error.name === "SequelizeValidationError") {
       book = Book.build(req.body);
@@ -83,17 +148,17 @@ router.post('/books', async(req, res) => {
 });
 
 /* POST deletes a book from the database */
-router.post('/books/:id/delete', async(req, res) => {
+router.post('/books/delete/:id', async(req, res) => {
   const book = await Book.findByPk(req.params.id);
   if (book) {
     await book.destroy();
-    res.redirect('/books');
+    res.redirect('/books/page-1');
   } else {
     const err = new Error;
     err.status = 404;
-    err.message = "Bummer, it looks like the book you are trying to delete doesn't exist."
+    err.message = "It looks like the book you are trying to delete doesn't exist."
     next(err);
   }
-})
+});
 
 module.exports = router;
